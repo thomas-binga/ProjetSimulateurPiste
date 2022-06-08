11adaaffbcef88d94d74a22f287534ccba47983b
thomas-binga <thomas.binga@gmail.com>
2022-06-08 11:16:29 +0200

    Initial commit

---
+++ src/main/java/com/projet/piste/PisteApplication.java
@@ -0,0 +1,13 @@
+package com.projet.piste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisteApplication.class, args);
+	}
+
+}
